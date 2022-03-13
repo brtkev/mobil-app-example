a66a6dd9f7567d9a2e64298ddbe3d8e1a01a5962
kev <kbreto2911@gmail.com>
2022-03-13 19:12:43 -0400

    add: pageWrappet to login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,27 +9,23 @@ import styles from './styles';
 
 import { RootStackParamList } from 'src/components/types'
 import { NativeStackScreenProps  } from '@react-navigation/native-stack';
+import PageWrapper from 'src/components/pageWrapper';
 type props = NativeStackScreenProps<RootStackParamList, 'Login'>;
 
 export default function Login({navigation} : props) {
 
 
 	return (
-		<View style={styles.mainContainer} >
-			<StatusBar />
-			<SafeAreaView  style={{flex:1}} ><ScrollView style={styles.scrollView} >
-				<View style={styles.contentBox} >
-					<Image source={require('assets/logo.png')} style={styles.logo} />
-					<TextApp style={{fontSize: 34, lineHeight: 36,marginBottom: 24}} >
-						Iniciar Sesión
-					</TextApp>
-					<LoginForm />
-					<Separator style={{marginBottom: 24, marginTop:32}} />
-					<LoginButton theme='social' social='google' style={{marginBottom: 16}} title='Continuar con Google' onPress={() => console.log("google press")} />
-					<LoginButton theme='social' social='facebook' style={{marginBottom: 44}} title='Continuar con Facebook' onPress={() => console.log("facebook press")} />
-					<GoToRegister onPress={() => navigation.navigate("Register")} />
-				</View>
-			</ScrollView></SafeAreaView>
-		</View>
+		<PageWrapper><View style={styles.contentBox} >
+			<Image source={require('assets/logo.png')} style={styles.logo} />
+			<TextApp style={{fontSize: 34, lineHeight: 36,marginBottom: 24}} >
+				Iniciar Sesión
+			</TextApp>
+			<LoginForm />
+			<Separator style={{marginBottom: 24, marginTop:32}} />
+			<LoginButton theme='social' social='google' style={{marginBottom: 16}} title='Continuar con Google' onPress={() => console.log("google press")} />
+			<LoginButton theme='social' social='facebook' style={{marginBottom: 44}} title='Continuar con Facebook' onPress={() => console.log("facebook press")} />
+			<GoToRegister onPress={() => navigation.navigate("Register")} />
+		</View></PageWrapper>
 	);
 }
