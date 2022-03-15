47b221bc813c99ed6f858bd5c35acd4b75343484
kev <kbreto2911@gmail.com>
2022-03-15 17:49:13 -0400

    add: select styles

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # whitepay-app
+
+
+## todo
+
+ - change down-pointer icon in select to the design width and heigth 
--- src/components/loginInput/select.tsx
+++ src/components/loginInput/select.tsx
@@ -1,7 +1,19 @@
-import { View, TouchableOpacity, TouchableOpacityProps } from "react-native"
+import { View, TouchableOpacity, TouchableOpacityProps, Image, StyleSheet } from "react-native"
 import colors from "src/styles/colors";
 import TextApp from "../textApp";
 import styles from './styles';
+
+const _styles = StyleSheet.create({
+  icon:{
+    height: 20,
+    width: 20,
+  },
+  iconContainer: {
+    position:"absolute", width: "auto", aspectRatio: 1,
+    top: 0, right: 0, bottom: 0,
+    display: "flex", alignItems: "center", justifyContent: "center",
+  }
+});
 
 interface Props extends TouchableOpacityProps {
   placeholder? : string;
@@ -14,6 +26,9 @@ const Select = (props : Props) => {
     <TouchableOpacity style={[{width:"100%"}, style]} {...TouchableProps}>
       <View style={ [styles.container]} >
         <TextApp style={{color:colors.textTerciary}}>{placeholder}</TextApp>
+        <View style={_styles.iconContainer}>
+          <Image style={_styles.icon} source={require('assets/icons/down-pointer.png')} />
+        </View>
       </View>
     </TouchableOpacity>
   )
